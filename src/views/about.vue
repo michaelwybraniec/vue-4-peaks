5917dcda2a239668ab4199b7b5d2d80b0afdeddd
Michael Wybraniec <46869143+michaelwybraniec@users.noreply.github.com>
2020-01-29 15:29:28 +0100

    About update

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,7 +1,26 @@
 <template>
-  <div class="content-container">
-    <div class="section content-title-group">
-      <h2 class="title">About Vue App 4 Peaks</h2>
+<div>
+<b-container>
+   <div class="text-center">
+      <h5 class="title">About Vue App 4 Peaks</h5>
     </div>
-  </div>
+  <b-row style="padding-top: 20px">
+    <b-row class="mt-4">
+      <b-col md="6" class="text-right">
+        <b-img src="https://media-exp1.licdn.com/dms/image/C5603AQFdGmGD1WjfZw/profile-displayphoto-shrink_200_200/0?e=1585785600&v=beta&t=kpR6bN1nw4L-UUfmrZ9UIzLBUQvxDpUWKGN1ZRBS_aQ" rounded="circle"></b-img>
+      </b-col>
+      <b-col md="6">
+        <b-card-body title="Hello!" style="padding-top: 45px;">
+          <b-card-text>
+            This is a demo VueJS app proptotyped at codesandbox.io
+          </b-card-text>
+        </b-card-body>
+      </b-col>
+    </b-row>
+  
+ 
+</b-row>
+
+</b-container>
+</div>
 </template>
