47334e5aec966616f41d96695fd43e68d18f616b
Michael Wybraniec <46869143+michaelwybraniec@users.noreply.github.com>
2020-02-03 18:48:03 +0100

    on API catch error => db.json loaded

--- src/components/header-bar-links.vue
+++ src/components/header-bar-links.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-   <b-navbar-nav right>
- <b-nav-item :href="github" right>
-  <i class="fab fa-github fa-2x" aria-hidden="true"></i>     
- </b-nav-item>
-  <b-nav-item :href="twitter" right>
-  <i class="fab fa-twitter fa-2x" style="padding-top: 2px" aria-hidden="true"></i>
-</b-nav-item >
- <b-nav-item :href="linkedin" right>
-  <i class="fab fa-linkedin fa-2x" aria-hidden="true"></i>     
- </b-nav-item>
-  </b-navbar-nav>
+    <b-navbar-nav right>
+      <b-nav-item :href="github" right>
+        <i class="fab fa-github fa-2x" aria-hidden="true"></i>
+      </b-nav-item>
+      <b-nav-item :href="twitter" right>
+        <i class="fab fa-twitter fa-2x" style="padding-top: 2px" aria-hidden="true"></i>
+      </b-nav-item>
+      <b-nav-item :href="linkedin" right>
+        <i class="fab fa-linkedin fa-2x" aria-hidden="true"></i>
+      </b-nav-item>
+    </b-navbar-nav>
   </div>
 </template>
 
@@ -18,11 +18,10 @@
 export default {
   data() {
     return {
-      github: 'https://github.com/michaelwybraniec',
-      twitter: 'https://twitter.com/mike_wybraniec',
-      linkedin: 'https://linkedin.com/michaelwybraniec/',
-      
+      github: "https://github.com/michaelwybraniec",
+      twitter: "https://twitter.com/mike_wybraniec",
+      linkedin: "https://linkedin.com/in/michaelwybraniec/"
     };
-  },
+  }
 };
 </script>
